6c3a2455a854773828af133d7ccb36fa8a071fda
NashaFrith <44258016+NashaFrith@users.noreply.github.com>
2026-04-01 17:00:41 -0500

    deployment prep

--- chessdude.py
+++ chessdude.py
@@ -3,8 +3,9 @@ import chess.engine
 import chess.polyglot
 import random
 import os
+import shutil
 
-STOCKFISH_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "stockfish.exe")
+STOCKFISH_PATH = shutil.which("stockfish") or os.path.join(os.path.dirname(os.path.abspath(__file__)), "stockfish.exe")
 
 OPENING_BOOK = {
     "e2e4":                                         ["e7e5", "c7c5", "e7e6", "c7c6"],
